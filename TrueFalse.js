f4f8e4a76801a4e4f30337e5e90bed354b60bf7a
Eve Lin <34218569+Eve-Lin@users.noreply.github.com>
2018-07-04 20:33:21 +0200

    Add files via upload

---
+++ TrueFalse.js
@@ -0,0 +1,12 @@
+function trueNumber(number){
+  var number;
+  if(number % 5==0) && number % 7==0{
+    console.log("true "+number);
+  }
+  else {
+    console.log("false "+number);
+  }
+};
+
+trueNumber("35");
+trueNumber("40");
